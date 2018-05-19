a3794b2eddf0dbad66b631204453ad9aeee7dbd5
Tuxik <jlahitte@gmail.com>
2018-05-19 23:02:04 +0000

    Création d'un module Node.js et script de test

--- ServoController.js
+++ ServoController.js
@@ -6,35 +6,56 @@
 	
 		//Initialisation du controller du servo sur le GPIO précisé en param.
 	  function ServoController(gpioNbr){
-		this.initialState = 600,	  // Valeur de l'angle par defaut (semble être la valeur la plus petite admissible
-		this.currentState = 600,
-		this.increment = 100, 
-		this.maxDegree = 2500, 
+		this.initialValue = 600,	  // Valeur de l'angle par defaut (semble être la valeur la plus petite admissible
+		this.currentValue = 600,
+		this.maxValue = 2500, 
 		this.motor = new Gpio(gpioNbr, {mode: Gpio.OUTPUT});
 	  }
-	ServoController.prototype.toInitialState = function() {
-	  console.log("Initial State");
-	  this.currentState = this.initialState;
-	  this.motor.servoWrite(this.currentState);
-	},
-	ServoController.prototype.toMaxDegree = function(){
-	  console.log("Max Degree");
-	  this.currentState = this.maxDegree;
-	  this.motor.servoWrite(this.currentState);
-	},
+	ServoController.prototype.toInitialValue = function() {
+	  console.log("Initial Value : " + this.initialValue);
+	  this.currentValue = this.initialValue;
+	  this.motor.servoWrite(this.currentValue);
+	}
+	ServoController.prototype.toMaxValue = function(){
+	  console.log("Max Value : " + this.maxValue);
+	  this.currentValue = this.maxValue;
+	  this.motor.servoWrite(this.currentValue);
+	}
 	ServoController.prototype.switchState = function(){
-		if (this.currentState === this.maxDegree){
-			this.toInitialState();
+		if (this.currentValue === this.maxValue){
+			this.toInitialValue();
 		}else{
-			this.toMaxDegree();
-		}
-	},
-	ServoController.prototype.getState = function(){
-		if (this.currentState === this.maxDegree){
-			return "maxDegree";
-		}else{
-			return "initial State";
+			this.toMaxValue();
 		}
 	}
-
+	ServoController.prototype.addRotate = function(value){
+		if ((value + this.currentValue) >= this.initialValue && (value + this.currentValue) <= this.maxValue){
+			this.currentValue += value;
+			this.motor.servoWrite(this.currentValue);
+		}
+	}
+	 ServoController.prototype.getValue = function(){
+		 return this.currentValue;
+		// if (this.currentValue === this.maxValue){
+			// return "maxDegree";
+		// }else{
+			// return "initial State";
+		// }
+	 }	
+	// ServoController.prototype.rotateOf = function(degrees){
+		// if (degrees && degrees !== undefined && degrees !== "" && degrees <= 180){
+			// var ratioDegrees = degrees / 180;
+			// console.log(ratioDegrees);
+			// var maxRotate = this.maxValue - this.initialValue;
+			// var ratioRotate = maxRotate * ratioDegrees;
+			// console.log(ratioRotate);
+			// var rotateValue = ratioRotate + this.initialValue;
+			// console.log(rotateValue);
+			// this.motor.servoWrite(rotateValue);
+		// }
+	// }
+	
+	ServoController.prototype.rotateOf90 = function(){
+		this.motor.servoWrite(1950);
+	}
 	module.exports = ServoController
--- ServoController_Test.js
+++ ServoController_Test.js
@@ -1,11 +1,23 @@
 //Script de test de ServoController
-
+//180° = 1900 
 
 var ServoController = require('./ServoController');
 var controller = new ServoController(10);//initialisation du gpio 10
+var direction = 1;
+ setInterval(function(){
+	 console.log("min " + controller.initialValue);
+	 console.log("max " + controller.maxValue);
+	 var val = controller.getValue();
+	  console.log("current " + val);
+	  if (val <= controller.initialValue){
+		  direction = Math.abs(direction);
+	  }
+	  if(val >= controller.maxValue){
+		direction = -1;
+	  }
+	  var finalValue = (475 * direction);
+	  console.log(finalValue);
+	  controller.addRotate(finalValue);
+ },500);
 
 
-setInterval(function(){
-	controller.switchState();
-	controller.getState();
-},1000);
